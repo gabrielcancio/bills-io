494517e2f3f9440225f174902bdda4128016e764
André Victor <victorandre280@gmail.com>
2024-02-19 23:18:08 -0300

    feat: Criação do prisma client e utilização no teste.ts

--- src/infra/http/routes/teste.ts
+++ src/infra/http/routes/teste.ts
@@ -1,6 +1,12 @@
 import { FastifyInstance } from "fastify";
-export async function teste(app:FastifyInstance){
-  app.get('/', async(request,reply)=>{
-    return {message:"Funcionando"}
+import { prisma } from '../../../infra/client/prisma'
+export async function teste(app: FastifyInstance) {
+  app.get('/', async (request, reply) => {
+    const data = await prisma.tabelaTeste.create({
+      data: {
+        nome: 'teste'
+      }
+    })
+    return reply.send(data)
   })
 }
